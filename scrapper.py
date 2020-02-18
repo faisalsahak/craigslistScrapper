03e03a8cf029ad41ff77b6bf1c78e7c1ec7c369f
user <user@users-MacBook-Pro.local>
2020-02-17 19:45:18 -0800

    imported the necessary libraries, and download the gecko driver

---
+++ scrapper.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+# from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+# binary = FirefoxBinary('/Applications/Firefox.app/Contents/MacOS/firefox')
+# browser = webdriver.Firefox(firefox_binary=binary)
+
+
+
+
+
+
+
+
+
+
+
+
